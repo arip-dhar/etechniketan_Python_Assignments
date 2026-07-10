7c85edcf804c59214224af22753b373a5ea3747b
Arip Dhar <aripdhar80@gmail.com>
2026-07-10 22:03:49 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-36. WAP that reads a single string from user and outputs a dictionary showing how many 
-# times each character appears in the string. 
-# Example: s1 = “google.com”, Output => {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1} 
-# Hints:  
-#  Initialize an empty dictionary 
-#  Iterate over each character in the input string.  
-#  For each character, update its count in the dictionary if the character exists OR 
-# add the character as key and its value as 1 
